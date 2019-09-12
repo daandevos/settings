e2671791b00dee0ac9515950099d351373dbd5d0
Daan <daan@tutanota.de>
2019-09-12 22:36:53 +0200

    Routes and menu item added

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('user/setting', 'UserSettingController@edit');
+    Route::patch('user/setting', 'UserSettingController@update');
+});
