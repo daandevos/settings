8f709c671b53f0d57b35aacc5164a416cabe3c7f
Daan <daan@tutanota.de>
2019-09-13 01:00:21 +0200

    Small cleanup

--- app/Http/Controllers/UserSettingController.php
+++ app/Http/Controllers/UserSettingController.php
@@ -13,48 +13,6 @@ use App\Http\Requests\StoreUserSetting;
 class UserSettingController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @return \Illuminate\View\View
@@ -87,16 +45,5 @@ class UserSettingController extends Controller
         return redirect()->back()->with([
             'success' => 'Successfully saved.',
         ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
